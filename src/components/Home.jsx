059973bc5cc2c522a42271b100c6267405e199c9
Pranesh <pranesh25joshi@gmail.com>
2024-03-15 20:08:30 +0530

    Project done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-[100%] pt-[28%] md:pt-[15%] pb-[20%]  w-full  bg-gradient-to-b from-black via-black to-gray-800"
+      className="h-[100%] pt-[28%] md:pt-[15%] pb-[30%]  w-full  bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
